2580a147f814b513aaa19448ee8535a9758328fc
ttyS3 <41882455+ttys3@users.noreply.github.com>
2023-08-29 14:36:48 +0800

    chore: update to nosqlbooster4mongo 8.0.15

--- src/nosqlbooster4mongo/update-sources.sh
+++ src/nosqlbooster4mongo/update-sources.sh
@@ -2,6 +2,6 @@
 
 set -eou pipefail
 
-fedpkg --name nosqlbooster4mongo new-sources --offline nosqlbooster4mongo-8.0.10.tar.gz
+fedpkg --name nosqlbooster4mongo new-sources --offline nosqlbooster4mongo-8.0.15.tar.gz
 
 
